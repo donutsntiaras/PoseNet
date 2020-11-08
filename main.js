a005301f20abc6e477d7e06620e0fd37b7838589
Donuts_n_Tiaras <61537524+donutsntiaras@users.noreply.github.com>
2020-11-08 09:37:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+noseX=0;
+noseY=0;
+
 function preload(){}
 
 function setup() {
@@ -18,13 +21,18 @@ function modelLoaded() {
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log("nose x =" + results[0].pose.nose.x);
-        console.log("nose y =" + results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nose x =" + noseX);
+        console.log("nose y =" + noseY);
     }
 }
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(noseX, noseY, 20);
 }
 
 function takeSnapshot() {
